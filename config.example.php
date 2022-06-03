a391f5304ec6556807d3aebc46c7724e802850af
Charles E. Lehner <charles.lehner@spruceid.com>
2022-06-03 10:23:56 -0400

    Add verification method URL to example config

--- config.example.php
+++ config.example.php
@@ -2,6 +2,7 @@
 $config = [
 	'hmac_secret' => '000000000000000000000000000000000000',
 	'key_did' => 'did:example:changeme',
+	'vm' => 'did:example:changeme#key-1',
 	'trusted_issuers' => [
 	],
 	'key_filename' => $_SERVER['HOME'].'/.config/didkit/key.jwk'
